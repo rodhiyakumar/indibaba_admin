375f9b2fde1753afddf6633761d13b3603cbe89d
rodhiyakumar <89126571+rodhiyakumar@users.noreply.github.com>
2026-05-23 23:24:22 +0530

    csrf fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->validateCsrfTokens();
         $middleware->alias([
             'Auth' => \App\Http\Middleware\Auth::class,
             'NoAuth' => \App\Http\Middleware\NoAuth::class,
+        ]);
+        $middleware->preventRequestForgery(except: [
+            '*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
